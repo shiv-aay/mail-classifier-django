774df7502d33aa817c3abebef55805a678a418af
Shivoy Dixit <shivoy4ndixit@gmail.com>
2020-12-10 23:25:52 +0530

    login issue fixed

--- mail_classifier/views.py
+++ mail_classifier/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request,'mail_classifier/home.html')
 
+@login_required(login_url="/accounts/login")
 def upload(request):
     return render(request,'mail_classifier/upload.html')
 
+@login_required(login_url="/accounts/login")
 def train(request):
     return render(request,'mail_classifier/train.html')  
 
+@login_required(login_url="/accounts/login")
 def result(request):
     return render(request,'mail_classifier/result.html')  
