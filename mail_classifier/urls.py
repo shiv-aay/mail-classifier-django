b79539932bb59db5d9fb92fc44dc297f7b1b4365
Shivoy Dixit <shivoy4ndixit@gmail.com>
2020-12-14 01:13:34 +0530

    file input and unzipping done

--- mail_classifier/urls.py
+++ mail_classifier/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,6 @@ urlpatterns = [
     path('train/',views.train,name='train'),
     path('result/',views.result,name='result'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mail_classifier/views.py
+++ mail_classifier/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+import zipfile
+import os
 
 def home(request):
     return render(request,'mail_classifier/home.html')
 
 @login_required(login_url="/accounts/login")
 def upload(request):
+    if request.method == 'POST':
+        ## Delete the previously stored training set
+        try:
+            os.remove('media/extracted/'+request.user.username)
+        except:
+            pass
+        category1_files = request.FILES['category1']
+        fs = FileSystemStorage()
+        filename = fs.save(category1_files.name, category1_files)
+        uploaded_file_url = fs.url(filename)
+        print("\n\n\nI'm here\n\n\n")
+        with zipfile.ZipFile(str(uploaded_file_url)[1:], 'r') as zip_ref:
+            zip_ref.extractall('media/extracted/'+request.user.username)
+
+
+        unzipped_files='media/extracted/request.user.username'            
+        return render(request, 'mail_classifier/upload.html', {
+            'uploaded_file_url': unzipped_files
+        })    
     return render(request,'mail_classifier/upload.html')
 
 @login_required(login_url="/accounts/login")
